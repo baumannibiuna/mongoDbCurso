122e786288f42fc3615428e70395b282e3596b96
baumannibiuna <baumannibiuna@gmail.com>
2022-09-15 16:34:25 -0300

    Projeto Criado

---
+++ src/main/java/com/baumannibiuna/mongoDbCurso/MongoDbCursoApplication.java
@@ -0,0 +1,13 @@
+package com.baumannibiuna.mongoDbCurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDbCursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baumannibiuna/mongoDbCurso/MongoDbCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baumannibiuna.mongoDbCurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
